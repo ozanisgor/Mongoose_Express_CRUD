f8513620b90fd36976f2db5118cab6b331192ff2
ozan isgor <ozanisgor11@gmail.com>
2022-07-06 15:14:40 +0300

    add: model (product) & seeds

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product');
+
 mongoose
-  .connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
