7b7b6e8b7af242bc7a2fdf964401cd64da7a3eed
ozan isgor <ozanisgor11@gmail.com>
2022-07-06 15:44:51 +0300

    add: index & route

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-  res.send('WOOF!');
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
 });
 
 app.listen(3000, () => {
